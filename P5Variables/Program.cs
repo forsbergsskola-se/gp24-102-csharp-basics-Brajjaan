7ae1071676e481260d34523ebf0a8da20c3a0cc3
Brajjaan <bryanvouriot@hotmail.com>
2024-09-16 13:20:31 +0200

    hej

--- P5Variables/Program.cs
+++ P5Variables/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿String a = "Hello";
+
+Console.WriteLine(a);
+
+a = "World";
+
+Console.WriteLine(a);
+
+string b = a;
+
+Console.WriteLine(b);
+
+a = "Forsbergs";
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+
+
+
+
