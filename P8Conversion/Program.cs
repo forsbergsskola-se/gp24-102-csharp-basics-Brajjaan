4a28077d393da40d71e44391e6221b1574b03724
Brajjaan <bryanvouriot@hotmail.com>
2024-09-17 10:13:55 +0200

    commit

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Give me a number");
+
+string input = Console.ReadLine();
+
+double d = Convert.ToDouble(input);
+
+Console.WriteLine(d);
+
+int i = Convert.ToInt32(input);
+Console.WriteLine(i);
+
+
+
+
+
